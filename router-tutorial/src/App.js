2774c4e86905a72efa62f37bf62f29109a49afb9
Tomady <scanliberte@gmail.com>
2023-09-19 16:13:08 +0900

    feat: Learn nested routes

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -4,15 +4,19 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile"
 import Article from "./pages/Article";
 import Articles from "./pages/Articles";
+import Layout from "./Layout"
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile />} />
-      <Route path="/articles" element={<Articles />} />
-      <Route path="/articles/:id" element={<Article />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
+      <Route path="/articles" element={<Articles />}>
+        <Route path="/articles/:id" element={<Article />} />
+      </Route>
     </Routes>
   );
 }
