9e3852840a91eabde5f009fd4670f79907f366ee
Tomady <scanliberte@gmail.com>
2023-10-19 22:45:14 +0900

    feat: loggerMiddleware

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
+import {createLogger} from 'redux-logger';
 
-const store = createStore(rootReducer);
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
